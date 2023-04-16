2b0df493aab3da2c4642f96660dc0e91d96fd94b
Hillel1408 <etemax@bk.ru>
2023-04-16 10:35:29 +0300

    calendar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import './css/null.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="calendar">
+            <div className="calendar__head">
+                <h1 className="calendar__title">Interview Calendar</h1>
+                <button className="calendar__head-btn">
+                    <svg
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M10.1683 8.35559H17.8633V9.90528H10.1683V17.6715H8.61859V9.90528H0.941406V8.35559H8.61859V0.571533H10.1683V8.35559Z"
+                            fill="#333333"
+                        />
+                    </svg>
+                </button>
+            </div>
+            <div className="calendar__date">
+                <div className="calendar__block">
+                    <span>M</span>
+                    <span>T</span>
+                    <span>W</span>
+                    <span>T</span>
+                    <span>F</span>
+                    <span>S</span>
+                    <span>S</span>
+                </div>
+                <div className="calendar__days">
+                    <span>25</span>
+                    <span>26</span>
+                    <span>27</span>
+                    <span>28</span>
+                    <span className="active">29</span>
+                    <span>30</span>
+                    <span>31</span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
